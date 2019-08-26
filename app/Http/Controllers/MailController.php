9c0e1194a79db96ffc4d3e6c53bd8a05d00868ce
Savepong <savepong.com@gmail.com>
2019-08-26 20:22:52 +0700

    Added: DATABASE QUEUE

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Mail;
-use App\Mail\OrderShipped;
+use App\Jobs\SendOrderEmail;
 use App\Order;
+use Log;
 
 class MailController extends Controller
 {
     public function index()
     {
         $order = Order::findOrFail(rand(1, 50));
+        SendOrderEmail::dispatch($order);
 
-        $recipient = 'mail.savepong@gmail.com';
-
-        Mail::to($recipient)->send(new OrderShipped($order));
-
-        return 'Sent order ' . $order->id;
+        Log::info('Dispatched order ' . $order->id);
+        return 'Dispatched order ' . $order->id;
     }
 }
---
+++ app/Jobs/SendOrderEmail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderShipped;
+use App\Order;
+use Log;
+
+class SendOrderEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // throw new \Exception("I am throwing this exception", 1);
+
+        $recipient = 'mail.savepong@gmail.com';
+        Mail::to($recipient)->send(new OrderShipped($this->order));
+        Log::info('Emailed order ' . $this->order->id);
+    }
+}
